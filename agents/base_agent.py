020bfa8a44d5d78f2edd11fc08cad409ebfb0cbd
Yosef Shammout <YosefShammout123@gmail.com>
2025-08-29 01:07:43 -0400

    little update regarding base agent structure

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -6,3 +6,11 @@
      └───────────────────── SharedMemory ────────────────────────┘
 """
 
+#interface
+
+class BaseAgent:
+    def __init__(self, agent_id, SharedMemory, policy, config=None):
+        self.agent_id = agent_id
+        self.sm = SharedMemory
+        self.policy = policy
+        self.config = config or {}
